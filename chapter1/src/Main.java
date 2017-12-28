abfb4325cd3687a32157d6567d54786eb215dcea
Jinyue Song <jsong32@dons.usfca.edu>
2017-12-27 20:02:13 -0500

    chapter 1 Q1
    boolean array initial values are false

--- chapter1/src/Main.java
+++ chapter1/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         UniqueChar uniqueChar = new UniqueChar();
         boolean res = uniqueChar.isUniqueChar2(input);
 
-        System.out.println(res);
+        System.out.println("Input string: " + input + ", is unique? " + res);
     }
 }

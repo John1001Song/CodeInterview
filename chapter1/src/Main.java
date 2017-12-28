35afe5dbc8c7d4973abef5089207eb50cc7c1c46
Jinyue Song <jsong32@dons.usfca.edu>
2017-12-27 22:40:35 -0500

    unique char method 2

--- chapter1/src/Main.java
+++ chapter1/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        String input = "ABC";
+        String input = "ABCA";
         UniqueChar uniqueChar = new UniqueChar();
         boolean res = uniqueChar.isUniqueChar2(input);
 
-        System.out.println("Input string: " + input + ", is unique? " + res);
+        boolean res1 = uniqueChar.isUniqueChar(input);
+
+        System.out.println("Input string in uniqueChar2: " + input + ", is unique? " + res);
+        System.out.println("Input string in uniqueChar: " + input + ", is unique? " + res1);
     }
 }
--- chapter1/src/UniqueChar.java
+++ chapter1/src/UniqueChar.java
@@ -1,5 +1,8 @@
 public class UniqueChar {
+
     public boolean isUniqueChar2(String string) {
+        System.out.println("========= isUniqueChar2 func =========");
+
         if (string.length() > 128) return false;
 
         // boolean array initial values are false
@@ -12,6 +15,38 @@ public class UniqueChar {
                 return false;
             }
             char_set[val] = true;
+        }
+        return true;
+    }
+
+    // assume input string is lower letters
+    // <<, &, and |: Bitwise and Bit Shift Operators
+    // the int type has a limited range, which affects the binary number range and make the result inaccurate. 
+    public boolean isUniqueChar(String string) {
+        System.out.println("========= isUniqueChar func =========");
+        int checker = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            int val = string.charAt(i) - 'a';
+            System.out.println("val: " + val);
+            if ((checker & (1 << val)) > 0) {
+                System.out.println("val after bit shift: " + val);
+                System.out.println("val in binary: " + Integer.toBinaryString(val));
+                System.out.println("(0 << val): " + Integer.toBinaryString(0 << val));
+                System.out.println("val: " + Integer.toBinaryString(val));
+                int old = 1000;
+                System.out.println("old: " + old);
+                System.out.println("old in binary: " + Integer.toBinaryString(old));
+                int testVal = 2 << old;
+                System.out.println("testVal: " + testVal);
+                System.out.println("testVal in binary: " + Integer.toBinaryString(testVal));
+                System.out.println("(1 << val): " + Integer.toBinaryString(1 << val));
+                System.out.println("(2 << val): " + Integer.toBinaryString(2 << val));
+
+                System.out.println("(checker & (1 << val)): " + (checker & (1 << val)));
+                return false;
+            }
+            checker |= (1 << val);
         }
         return true;
     }

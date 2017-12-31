5cc4e02478ae32415e889e335d5d490b7790a94e
Jinyue Song <jsong32@dons.usfca.edu>
2017-12-31 02:54:21 -0500

    q8

--- chapter1/src/Question8.java
+++ chapter1/src/Question8.java
@@ -1,3 +1,7 @@
 public class Question8 {
-
+    // my solution idea is quite simple:
+    //  attach the string s1's head to itself tail, which is s1s1.
+    //  then, call the func isSubstring to check s1s1 and s2.
+    // the difficult part is how to implement the func isSubstring.
+    
 }

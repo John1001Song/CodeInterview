b7ca2b36e034fde41b9b4ffdbd913ad700c5180b
Jinyue Song <jsong32@dons.usfca.edu>
2017-12-31 01:13:06 -0500

    q7

---
+++ chapter1/src/Question7.java
@@ -0,0 +1,87 @@
+import static java.lang.Math.random;
+
+public class Question7 {
+
+    // the provided code is easy to read and understand
+    // the idea is clear:
+    //  set up two boolean arrays
+    //  go through all elements and if there is a zero, then locate the index in boolean arrays to be true
+    //  based on the boolean arrays, nullify the matrix
+    // the time complexity is O(m*n) <= m * n + m + n
+    // the space complexity is O(m) or O(n) whichever is bigger
+
+    public static void setZeros(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        // store the row and column index with value 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        // nullify rows
+        for (int i = 0; i < row.length; i++) {
+            if (row[i]) {
+                nullifyRow(matrix, i);
+            }
+        }
+
+        for (int j = 0; j < column.length; j++) {
+            if (column[j]) {
+                nullifyColumn(matrix, j);
+            }
+        }
+    }
+
+    public static void nullifyRow(int[][] matrix, int row) {
+        for (int j = 0; j < matrix[0].length; j++) {
+            matrix[row][j] = 0;
+        }
+    }
+
+    public static void nullifyColumn(int[][] matrix, int column) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][column] = 0;
+        }
+    }
+
+    public static void main(String args[]) {
+        // create a matrix
+        int rowLength = 3;
+        int columnLength = 4;
+        int[][] matrix = new int[rowLength][columnLength];
+
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < columnLength; j++) {
+                matrix[i][j] = (int) (Math.random()*10);
+            }
+        }
+
+        matrix[1][2] = 0;
+
+        System.out.println("=======original matrix:=======");
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < columnLength; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("=======setZeros matrix:=======");
+        setZeros(matrix);
+
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < columnLength; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
